b46073cd16d897689e7bc8406da2357815d590c3
ownedit <ownedit@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-22 14:23:04 +0000

    updating styles
    git-svn-id: https://plugins.svn.wordpress.org/woocommerce-ownedit/trunk@1185571 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- Owned_it_options.php
+++ Owned_it_options.php
@@ -35,7 +35,7 @@ button {
     <h2>Owned it &raquo; Settings</h2>
 	<p class="inf"> Please enter your store ID, a store ID is generated for each store you add to Owned it and this can be found on Owned it Dashboard -> Account Settings -> Store Settings Tab.
 		</p>
-    <table width="550" border="0" cellpadding="5" cellspacing="5" style="margin-top:100px;margin-left:50px;"> 
+    <table width="550" border="0" cellpadding="5" cellspacing="5" style="margin-top:25px;margin-left:50px;"> 
 	<tr>
         <td id="key-holder" width="366" style="padding:5px;"><input placeholder="Please enter your store ID" id="storeid" name="<?php echo $this->plugin_id; ?>[storeid]" type="text" value="<?php echo $options['storeid']; ?>" style="margin:0px;width:100%;" /></td>
 		 <td > <input type="submit" name="submit" value="Save" class="button-primary"/></td>
